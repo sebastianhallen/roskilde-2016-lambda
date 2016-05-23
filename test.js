887ff3bd20fac6c723a970e6c61b595453d29c34
Martin Mazur <m.m@upplopp.se>
2016-05-23 17:31:59 +0200

    Simplified test code

--- test.js
+++ test.js
@@ -3,66 +3,27 @@
 const roskildeFunction = require('./index');
 const assert = require('chai').assert;
 
+function testDefaultRoskildeCommand(text, done) {
+    roskildeFunction.handler({
+      text: text
+    }, {
+      succeed: data => { 
+        assert(data.text !== 'Missing text!');
+        assert(data.attachments.length !== 0, 'Has no attachments!')
+        done(); 
+      },
+      fail: error => { 
+        done(error); 
+      }
+    });
+}
+
+
 describe('Roskilde', function() { 
   this.timeout(15000);
 
-  it('should get acts by day', (done) => {
-    roskildeFunction.handler({
-      text: 'day+Friday'
-    }, {
-      succeed: data => { 
-        assert(data.text !== 'Missing text!');
-        assert(data.attachments.length !== 0, 'Has no attachments!')
-        done(); 
-      },
-      fail: error => { 
-        done(error); 
-      }
-    });
-  });
-
-  it('should get acts by shorthand day', (done) => {
-    roskildeFunction.handler({
-      text: 'day+sat'
-    }, {
-      succeed: data => { 
-        assert(data.text !== 'Missing text!');
-        assert(data.attachments.length !== 0, 'Has no attachments!')
-        done(); 
-      },
-      fail: error => { 
-        done(error); 
-      }
-    });
-  });
-
-  it('should get acts by stage', (done) => {
-    roskildeFunction.handler({
-      text: 'stage+Apollo'
-    }, {
-      succeed: data => { 
-        assert(data.text !== 'Missing text!');
-        assert(data.attachments.length !== 0, 'Has no attachments!')
-        done(); 
-      },
-      fail: error => { 
-        done(error); 
-      }
-    });
-  });
-
-  it('should get details for artist', (done) => {
-    roskildeFunction.handler({
-      text: 'whois+UNCLE+ACID+&+THE+DEADBEATS'
-    }, {
-      succeed: data => { 
-        assert(data.text !== 'Missing text!');
-        assert(data.attachments.length !== 0, 'Has no attachments!')
-        done(); 
-      },
-      fail: error => { 
-        done(error); 
-      }
-    });
-  });
+  it('should get acts by day', done => testDefaultRoskildeCommand('day+Friday', done));
+  it('should get acts by shorthand day', done => testDefaultRoskildeCommand('day+sat', done));
+  it('should get acts by stage', done => testDefaultRoskildeCommand('stage+Apollo', done));
+  it('should get details for artist', done => testDefaultRoskildeCommand('whois+UNCLE+ACID+&+THE+DEADBEATS', done));
 });
